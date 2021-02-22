755d0f97240fd0f59ba9a83ac86a8a1b21961e87
Tobias Zillmer <nqxdev@gmail.com>
2021-02-22 21:46:16 +0100

    login btn added

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,13 @@ import Modal from './Modal';
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
+
+    let isLoggedIn = true;
+
+
     
     return (
         <div className="header">
-            <Modal open={isOpen} onClose={() => setIsOpen(false) } />
 
             <Link to="/">
                 <img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg" alt="" />
@@ -28,7 +31,29 @@ function Header() {
                 <p>Become a host</p>
                 <LanguageIcon />
                 <ExpandMoreIcon />
-                <Avatar onClick={() => setIsOpen(true) } />
+
+
+                { !isLoggedIn && <div>
+                        <Avatar className="avatar-icon" onClick={() => setIsOpen(true) } />
+                        <Modal open={isOpen} onClose={() => setIsOpen(false) } />
+                    </div> }
+                        
+                { isLoggedIn && <div style={{position: 'relative'}}> 
+                        <div className="avatar-box">
+                        <svg className="burger-menu" viewBox="0 0 100 80" width="15" height="15">
+                            <rect width="100" height="15"></rect>
+                            <rect y="30" width="100" height="15"></rect>
+                            <rect y="60" width="100" height="15"></rect>
+                        </svg>
+                        <Avatar className="avatar-icon" onClick={() => setIsOpen(true) } /> 
+                    </div>
+                        <div className="dropdown">
+                            <div className="dropdown-item">Nachrichten</div>
+                            <div className="dropdown-item">Benachrichtigungen</div>
+                            <div className="dropdown-item">Reisen</div>
+                        </div>
+                    </div> }
+
             </div>
         </div>
     )
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Button } from '@material-ui/core'
+
+import '../style/Modal.css'
 
 
 const MODAL_STYLES = {
     position: 'fixed',
+    width: '450px',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     backgroundColor: '#FFF',
     padding: '50px',
-    zIndex: 1000
+    zIndex: 1000,
+    borderRadius: '10px'
 }
 
 const OVERLAY_STYLES = {
@@ -28,10 +33,28 @@ function Modal({open, onClose}) {
     return ReactDom.createPortal(
         <>
         <div style={OVERLAY_STYLES}/>
+
         <div style={MODAL_STYLES}>
-            <h1>Modal</h1>
-            <button onClick={onClose}>Close</button>
+            <span class="btn-close" onClick={onClose}>&#x2715;</span>
+            <h1>Register</h1>
+            <form className="register-form">
+                <div className="input-top">
+                    <input className="input" type="text" placeholder="Email" />
+                </div>
+                <div className="input-bottom">
+                    <input className="input" type="text" placeholder="Password" />
+                </div>
+                <p>
+                    Wir werden dich anrufen oder dir eine SMS senden, um deine Nummer zu 
+                    bestätigen. Es können die üblichen Gebühren für die Nachrichten- und 
+                    Datenübertragung anfallen.
+                </p>
+                <Button>Weiter</Button>
+            </form>
+            <p>Hast du bereits ein Nutzerkonto?</p>
+            <a href="#">Register</a>
         </div>
+        
         </>,
         document.getElementById('portal')
     )
